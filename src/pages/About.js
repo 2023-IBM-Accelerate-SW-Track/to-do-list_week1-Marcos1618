0660bbf00e531c215b42b21a3664fd83af62e6b8
Marcos1618 <104402249+Marcos1618@users.noreply.github.com>
2023-06-08 22:35:45 -0500

    added splits

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
+/* import pfp from  pathname; */
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+        <div className="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Marcos Barrera</div>
+            <div className="brief_description">
+              // Details about you goes here
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
