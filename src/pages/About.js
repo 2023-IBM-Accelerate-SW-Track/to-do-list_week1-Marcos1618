11f31bf4d17df423eb06f89707bf94072b60a98b
Marcos1618 <104402249+Marcos1618@users.noreply.github.com>
2023-06-08 23:06:47 -0500

    not much

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 import "./About.css";
 /* import pfp from  pathname; */
-  
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div>
-        <div className="split left">
-          <div className="centered">
-            <img 
-              className="profile_image"
-              // Image goes here
-              alt="Profile Pic"
-              ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Marcos Barrera</div>
-            <div className="brief_description">
-              // Details about you goes here
+        <p>
+          <div>
+            <div className="split left">
+              <div className="centered">
+                <img
+                  className="profile_image"
+                  src={profile_pic}
+                  alt="Profile Pic"
+                // Image goes here
+                ></img>
+              </div>
+            </div>
+            <div className="split right">
+              <div className="centered">
+                <div className="name_title">Marcos Barrera</div>
+                <div className="brief_description">
+                  // I am a computer science student attending the University of Memphis
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        </p>
       </div>
     )
   }
